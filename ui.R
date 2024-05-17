8bc97adfc21e33f34e89fb31a090c9295b98731d
Stephanie Reinders <reinders.stephanie@gmail.com>
2024-05-17 12:46:33 -0500

    Turned off the global footer
    The global footer is not in the most recent mock-ups from Jeni so I turned it off in the app

--- ui.R
+++ ui.R
@@ -55,12 +55,13 @@ ui <- shinyUI({
             tabPanel("Instructions",),
             tabPanel("Contact",)
   ))),
-  # Footer
-  tags$div(id="global-footer",
-    fluidRow(
-      column(width = 4,tags$img(src="csafe_tools_blue_h.png", alt="Logo", height = "40px")),
-      column(width = 4,tags$p("195 Durham Center, 613 Morrill Road, Ames, Iowa, 50011")),
-      column(width = 4,tags$p("(C) 2023 | All Rights Reserved", class="right-float"))
-    )
-  ))  
+  # # Footer
+  # tags$div(id="global-footer",
+  #   fluidRow(
+  #     column(width = 4,tags$img(src="csafe_tools_blue_h.png", alt="Logo", height = "40px")),
+  #     column(width = 4,tags$p("195 Durham Center, 613 Morrill Road, Ames, Iowa, 50011")),
+  #     column(width = 4,tags$p("(C) 2023 | All Rights Reserved", class="right-float"))
+  #   )
+  # )
+  )  
 })
