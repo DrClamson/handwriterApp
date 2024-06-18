cc0a0168390380f294686ceee1da353bc4e27fea
Stephanie Reinders <reinders.stephanie@gmail.com>
2024-06-18 15:38:45 -0500

    Moved main directory functionality to module

---
+++ R/maindir.R
@@ -0,0 +1,58 @@
+maindirUI <- function(id) {
+  ns <- NS(id)
+  tagList(
+    shinyDirButton(ns("main_dir"), "Main folder", "Select a folder"),
+    verbatimTextOutput(ns("dir"), placeholder = TRUE)
+  )
+}
+
+
+maindirServer <- function(id, global) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      
+      shinyDirChoose(
+        input,
+        'main_dir',
+        roots = c(home = '~'),
+        filetypes = c('', 'txt', 'bigWig', "tsv", "csv", "bw")
+      )
+      
+      dir <- reactive(input$main_dir)
+      
+      # display folder path below button
+      output$dir <- renderText({
+        global$main_dir
+      })
+      
+      # update main directory to the selected directory
+      observeEvent(ignoreInit = TRUE,
+                   eventExpr = {
+                     input$main_dir
+                   },
+                   handlerExpr = {
+                     # update main directory
+                     if (!"path" %in% names(dir())) {
+                       return()
+                     }
+                     home <- normalizePath("~")
+                     global$main_dir <- file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep))
+                     
+                     # setup main directory or load previous analysis
+                     if (length(list.files(global$main_dir)) == 0){
+                       # setup directory for new analysis
+                       setup_main_dir(global$main_dir)
+                     } else {
+                       # load files if they exist to continue previously started analysis
+                       global$known_docs <- list_model_docs(global$main_dir, output_dataframe = TRUE)
+                       global$model <- load_model(global$main_dir)
+                       global$qd_path <- list_qd(global$main_dir)
+                       global$qd_image <- load_qd(global$qd_path)
+                       global$doc <- load_processed_qd(global$main_dir)
+                       global$analysis <- load_analysis(global$main_dir)
+                     }
+                   })
+    }
+  )
+}
