d4e2608c0dc4dc5eafe844a2831dc6faf0bf742e
Stephanie Reinders <reinders.stephanie@gmail.com>
2024-07-15 12:40:13 -0500

    Copied footer from BulletAnalyzR

--- R/app.R
+++ R/app.R
@@ -5,38 +5,46 @@ handwriterApp <- function(...){
   
   ui <- shiny::shinyUI({
     shiny::fluidPage(title = "handwriter",
-              shinyjs::useShinyjs(),
-              shiny::tags$head(
-                shiny::tags$link(
-                  href = "https://fonts.googleapis.com/css?family=Montserrat:400,500,700,900|Ubuntu:400,500,700",
-                  rel = "stylesheet",
-                  type = "text/css"
-                ),
-                # tags$link(rel = "shortcut icon", href = "favicon.png", type = "image/png"),
-                # tags$link(rel = "icon", href = "favicon.png", type = "image/png")
-              ),
-              shiny::includeCSS(system.file("extdata", "styles.css", package = "handwriterApp")),
-              shiny::tags$div(id="app-container",
-                       shiny::fluidRow(
-                         shiny::column(width = 4, shiny::tags$a(target = "_blank", href="https://forensicstats.org", shiny::tags$img(src = "images/CSAFE_Tools_handwriter_cropped.png", height="100px"))),
-                         shiny::column(width = 4, shiny::br()),
-                         shiny::column(width = 4, shiny::tags$a(target = "_blank", href="https://forensicstats.org", shiny::tags$img(src = "images/handwriter_graphic.png", height="100px"), class="right-float")),
+                     shinyjs::useShinyjs(),
+                     shiny::tags$head(
+                       shiny::tags$link(
+                         href = "https://fonts.googleapis.com/css?family=Montserrat:400,500,700,900|Ubuntu:400,500,700",
+                         rel = "stylesheet",
+                         type = "text/css"
                        ),
-                       shiny::tags$div(id="main-content",
-                                shiny::navbarPage(
-                                  shiny::tags$script(shiny::HTML("var header = $('.navbar > .container-fluid'); header.append('<div style=\"float:right\"><a href=\"https://forensicstats.org\"><img src=\"images/CSAFE-Tools_Stacked_white_cropped.png\" alt=\"alt\" style=\"float:right;width:117px;height:50px;padding-right:5px;\"> </a></div>'); console.log(header)")),
-                                  shiny::tabPanel("Home",
-                                           innerUI('inner1'),
-                                  ),
-                                  shiny::tabPanel( 
-                                    "About",
-                                    shiny::includeHTML(system.file(file.path("extdata", "HTML"), "about.HTML", package = "handwriterApp"))
-                                  ),
-                                  shiny::tabPanel(
-                                    "Contact",
-                                    shiny::includeHTML(system.file(file.path("extdata", "HTML"), "contact.HTML", package = "handwriterApp"))
-                                  )
-                                )))
+                       # tags$link(rel = "shortcut icon", href = "favicon.png", type = "image/png"),
+                       # tags$link(rel = "icon", href = "favicon.png", type = "image/png")
+                     ),
+                     shiny::includeCSS(system.file("extdata", "styles.css", package = "handwriterApp")),
+                     shiny::tags$div(id="app-container",
+                                     shiny::fluidRow(
+                                       shiny::column(width = 4, shiny::tags$a(target = "_blank", href="https://forensicstats.org", shiny::tags$img(src = "images/CSAFE_Tools_handwriter_cropped.png", height="100px"))),
+                                       shiny::column(width = 4, shiny::br()),
+                                       shiny::column(width = 4, shiny::tags$a(target = "_blank", href="https://forensicstats.org", shiny::tags$img(src = "images/handwriter_graphic.png", height="100px"), class="right-float")),
+                                     ),
+                                     shiny::tags$div(id="main-content",
+                                                     shiny::navbarPage(
+                                                       shiny::tags$script(shiny::HTML("var header = $('.navbar > .container-fluid'); header.append('<div style=\"float:right\"><a href=\"https://forensicstats.org\"><img src=\"images/CSAFE-Tools_Stacked_white_cropped.png\" alt=\"alt\" style=\"float:right;width:117px;height:50px;padding-right:5px;\"> </a></div>'); console.log(header)")),
+                                                       shiny::tabPanel("Home",
+                                                                       innerUI('inner1'),
+                                                       ),
+                                                       shiny::tabPanel( 
+                                                         "About",
+                                                         shiny::includeHTML(system.file(file.path("extdata", "HTML"), "about.HTML", package = "handwriterApp"))
+                                                       ),
+                                                       shiny::tabPanel(
+                                                         "Contact",
+                                                         shiny::includeHTML(system.file(file.path("extdata", "HTML"), "contact.HTML", package = "handwriterApp"))
+                                                       )
+                                                     ))),
+                     # Footer
+                     tags$div(id="global-footer",
+                              fluidRow(
+                                column(width = 4,tags$img(src="images/csafe_tools_blue_h.png", alt="Logo", height = "40px")),
+                                column(width = 4,tags$p("195 Durham Center, 613 Morrill Road, Ames, Iowa, 50011")),
+                                column(width = 4,tags$p("(C) 2023 | All Rights Reserved", class="right-float"))
+                              )
+                     )
     )  
   })
   
