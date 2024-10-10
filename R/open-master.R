b30ed7944368030f19bb9be8b03fd28f75cc9aaf
Stephanie Reinders <reinders.stephanie@gmail.com>
2024-10-10 14:17:28 -0500

    Calculate and display SLR

--- R/open-master.R
+++ R/open-master.R
@@ -27,9 +27,9 @@ openUI <- function(id) {
                                                                                        ns = shiny::NS(id),
                                                                                        shiny::div(id = "autonomous",
                                                                                                   format_sidebar(title = "WELCOME",
-                                                                                                                 help_text = "Start using handwriter to compare two handwritten documents."),
-                                                                                                  # shiny::fluidRow(shiny::column(width = 3, shiny::actionButton(ns("demo_button"), "Demo")),
-                                                                                                  #                 shiny::column(width = 9, align = "right", shiny::actionButton(ns("case_button"), align="right", "Casework Simulation")))
+                                                                                                                 help_text = NULL,
+                                                                                                                 module = shiny::tagList(
+                                                                                                                   openSampleSidebarUI(ns('samples'))))
                                                                                        ),
                                                                ),
                                                                
@@ -44,22 +44,8 @@ openUI <- function(id) {
                          # Welcome Display ----
                          shiny::tabPanel(id = ns("Welcome"),
                                          title = "Welcome",
-                                         shiny::h3("WELCOME TO HANDWRITER!"),
-                                         shiny::p("Unlock the power of handwriting analysis with handwriter. 
-                                                This tool is designed to assist forensic examiners by analyzing handwritten 
-                                                documents against a closed set of potential writers. It determines the probability 
-                                                that each writer wrote the document. Whether you are a forensic document examiner, 
-                                                legal professional, academic, or simply curious about how statistics are applied to 
-                                                handwriting, handwriter provides an automated way to evaluate handwriting samples."),
-                                         shiny::br(),
+                                         shinycssloaders::withSpinner(openSampleBodyUI(ns('samples')))
                          ),
-                         
-                         # Demo Display ----
-                         shiny::tabPanel(id = ns("Demo Preview"),
-                                         title = "Demo Preview",
-                                         shinycssloaders::withSpinner(demoPreviewBodyUI(ns('demo_preview')))
-                         ),
-                         
       )
     )
     )
@@ -71,109 +57,15 @@ openServer <- function(id){
   shiny::moduleServer(
     id,
     function(input, output, session){
-      # NEXT BUTTONS ----
-      # disable next buttons at start
-      shinyjs::disable("demo_known_next_button")
-      shinyjs::disable("case_project_next_button")
-      shinyjs::disable("case_known_next_button")
-      shinyjs::disable("case_qd_next_button")
-      
-      # enable next buttons
-      shiny::observe({
-        # main_dir needs to be defined
-        shiny::req(global$main_dir)
-        shinyjs::enable("case_project_next_button")
-      })
-      shiny::observe({
-        # model needs to be loaded
-        shiny::req(global$model)
-        shinyjs::enable("case_known_next_button")
-        shinyjs::enable("demo_known_next_button")
-      })
-      shiny::observe({
-        # analysis needs to be loaded
-        shiny::req(global$analysis)
-        shinyjs::enable("case_qd_next_button")
-      })
-      
-      # demo next buttons
-      shiny::observeEvent(input$demo_button, {shiny::updateTabsetPanel(session, "screen", selected = "Demo Preview")})
-      shiny::observeEvent(input$demo_preview_next_button, {shiny::updateTabsetPanel(session, "screen", selected = "Demo Known")})
-      shiny::observeEvent(input$demo_known_next_button, {shiny::updateTabsetPanel(session, "screen", selected = "Demo QD")})
-      shiny::observeEvent(input$demo_qd_next_button, {shiny::updateTabsetPanel(session, "screen", selected = "Welcome")})
-      
-      # casework next buttons
-      shiny::observeEvent(input$case_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Requirements")})
-      shiny::observeEvent(input$case_requirements_next_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Files")})
-      shiny::observeEvent(input$case_files_next_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Project")})
-      shiny::observeEvent(input$case_project_next_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Known")})
-      shiny::observeEvent(input$case_known_next_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Questioned")})
-      shiny::observeEvent(input$case_qd_next_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Report")})
-      
-      # demo back buttons
-      shiny::observeEvent(input$demo_preview_back_button, {shiny::updateTabsetPanel(session, "screen", selected = "Welcome")})
-      shiny::observeEvent(input$demo_known_back_button, {shiny::updateTabsetPanel(session, "screen", selected = "Demo Preview")})
-      shiny::observeEvent(input$demo_qd_back_button, {shiny::updateTabsetPanel(session, "screen", selected = "Demo Known")})
-      
-      # casework back buttons
-      shiny::observeEvent(input$case_requirements_back_button, {shiny::updateTabsetPanel(session, "screen", selected = "Welcome")})
-      shiny::observeEvent(input$case_files_back_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Requirements")})
-      shiny::observeEvent(input$case_project_back_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Files")})
-      shiny::observeEvent(input$case_known_back_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Project")})
-      shiny::observeEvent(input$case_qd_back_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Known")})
-      shiny::observeEvent(input$case_report_back_button, {shiny::updateTabsetPanel(session, "screen", selected = "Case Questioned")})
-      
+
       # STORAGE ----
-      global <- shiny::reactiveValues(
-        analysis = NULL,
-        known_names = NULL,
-        known_paths = NULL,
-        main_dir = NULL,
-        model = NULL,
-        qd_names = NULL,
-        qd_paths = NULL
+      open_global <- shiny::reactiveValues(
+        sample1_path = NULL,
+        sample2_path = NULL
       )
       
-      # Reset storage
-      shiny::observeEvent(input$demo_button, {
-        reset_app(global)
-        delete_demo_dir()
-      })
-      
-      # Reset storage
-      shiny::observeEvent(input$case_button, {
-        reset_app(global)
-        delete_demo_dir()
-      })
-      
-      # Reset storage and empty temp > demo directory
-      shiny::observeEvent(input$demo_qd_next_button, {
-        reset_app(global)
-        delete_demo_dir()
-      })
-      
-      
-      # DEMO PREVIEW ----
-      demoPreviewServer('demo_preview', global)
-      
-      # DEMO KNOWN ----
-      demoKnownServer('demo_known', global)
-      
-      # DEMO QD ----
-      demoQDServer('demo_qd', global)
-      
-      # MAIN DIRECTORY ----
-      caseMaindirServer('case_maindir', global)
-      
-      # KNOWN WRITING ----
-      caseKnownServer('case_known', global)
-      
-      # QUESTIONED DOCUMENT ----
-      caseQDServer('case_qd', global)
-      
-      # REPORT ----
-      caseReportServer('case_report', global)
-      
+      # WELCOME ----
+      openSampleServer('samples', open_global)
     }
   )
 }
---
+++ R/open-samples.R
@@ -0,0 +1,55 @@
+openSampleSidebarUI <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::tagList(
+    shiny::helpText("Select two scanned handwriting samples saved as a PNG images."),
+    shiny::fileInput(ns("open_upload1"), "Sample 1", accept = ".png", multiple=FALSE),
+    shiny::fileInput(ns("open_upload2"), "Sample 2", accept = ".png", multiple=FALSE)
+  )
+}
+
+openSampleBodyUI <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::tagList(
+    shinycssloaders::withSpinner(shiny::uiOutput(ns("slr_results")))
+  )
+}
+
+openSampleServer <- function(id, open_global) {
+  shiny::moduleServer(
+    id,
+    function(input, output, session) {
+      
+      shiny::observeEvent(input$open_upload1, {
+        path <- input$open_upload1$datapath
+        open_global$sample1_path <- path
+      })
+      
+      shiny::observeEvent(input$open_upload2, {
+        # reset slr
+        open_global$slr <- NULL
+        
+        path <- input$open_upload2$datapath
+        open_global$sample2_path <- path
+        
+        open_global$slr <- handwriterRF::calculate_slr(
+          sample1_path = open_global$sample1_path,
+          sample2_path = open_global$sample2_path)
+      })
+      
+      output$slr <- shiny::renderText({
+        open_global$slr
+      })
+      
+      output$slr_results <- shiny::renderUI({
+        req(open_global$slr)
+        ns <- session$ns
+        shiny::tagList(
+          shiny::h3("Comparison Results"),
+          shiny::HTML("Score-based Likelihood Ratio"),
+          shiny::textOutput(ns("slr")),
+          shiny::br()
+        )
+      })
+    }
+  )
+}
